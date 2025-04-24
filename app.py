8fb9ee464c8eac4e753ead8d18a3b71947765667
Elsa Chung <elsachung999@gmail.com>
2025-04-24 21:17:40 +0800

    revise to algorithm=HS256

--- app.py
+++ app.py
@@ -278,7 +278,7 @@ def create_jwt_token(user_id,name,email):
         "name":name,
         "email":email,        
     }
-    token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
     return token
 
 class acctInfo(BaseModel):    
